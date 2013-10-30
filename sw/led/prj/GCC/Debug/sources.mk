8393cc99efcc2440e294643cf70c7cbb845a0fe5
kondal2000 <kondal2000@gmail.com>
2013-10-30 12:18:19 +0900

    add new

---
+++ sw/led/prj/GCC/Debug/sources.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+OBJS := 
+C_DEPS := 
+EXECUTABLES := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+prj/MDK-ARMv4 \
+prj/GCC/Debug \
+prj/GCC/Debug/Libraries/STM32_USB-FS-Device_Driver/src \
+prj/GCC/Debug/Libraries/STM32F10x_StdPeriph_Driver/src \
+prj/GCC/Debug/Libraries/CMSIS/Device/ST/STM32F10x/Source/Templates \
+prj/GCC/Debug/Libraries/CMSIS/Device/ST/STM32F10x/Source/Templates/TrueSTUDIO \
+. \
+STM32F10x_StdPeriph_Driver/src \
+CMSIS/Device/ST/STM32F10x/Source/Templates \
+CMSIS/Device/ST/STM32F10x/Source/Templates/TrueSTUDIO \
+
---
+++ sw/led/prj/GCC/Debug/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+D:/svn/google_code/jk-stm32f103-dragon/sw/led/hw_config.c \
+D:/svn/google_code/jk-stm32f103-dragon/sw/led/main.c 
+
+OBJS += \
+./hw_config.o \
+./main.o 
+
+C_DEPS += \
+./hw_config.d \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+hw_config.o: D:/svn/google_code/jk-stm32f103-dragon/sw/led/hw_config.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	arm-none-eabi-gcc -DUSE_STDPERIPH_DRIVER -DSTM32F10X_HD -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../lib\STM32_USB-FS-Device_Lib_V3.4.0\Libraries\CMSIS\Device\ST\STM32F10x\Include" -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../lib\STM32_USB-FS-Device_Lib_V3.4.0\Libraries\STM32_USB-FS-Device_Driver\inc" -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../lib\STM32_USB-FS-Device_Lib_V3.4.0\Libraries\STM32F10x_StdPeriph_Driver\inc" -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../led" -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../lib/STM32_USB-FS-Device_Lib_V3.4.0/Libraries/CMSIS/Include" -O0 -g3 -Wall -c -v -mthumb -mcpu=cortex-m3 -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+main.o: D:/svn/google_code/jk-stm32f103-dragon/sw/led/main.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	arm-none-eabi-gcc -DUSE_STDPERIPH_DRIVER -DSTM32F10X_HD -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../lib\STM32_USB-FS-Device_Lib_V3.4.0\Libraries\CMSIS\Device\ST\STM32F10x\Include" -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../lib\STM32_USB-FS-Device_Lib_V3.4.0\Libraries\STM32_USB-FS-Device_Driver\inc" -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../lib\STM32_USB-FS-Device_Lib_V3.4.0\Libraries\STM32F10x_StdPeriph_Driver\inc" -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../led" -I"D:/svn/google_code/jk-stm32f103-dragon/sw/led/prj/GCC/../../../lib/STM32_USB-FS-Device_Lib_V3.4.0/Libraries/CMSIS/Include" -O0 -g3 -Wall -c -v -mthumb -mcpu=cortex-m3 -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
